e8e56a6aeedd761e8c4f798027270703b6849ce9
Mohamed EL HAMDY <mohamedelhamdy7@gmail.com>
2025-02-09 20:07:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,18 @@
 # app.py
-
 import streamlit as st
-
-
 
 st.set_page_config(page_title="Plateforme Économie & Finance", layout="wide")
 
-
-
 st.title("Plateforme Interactive d’Apprentissage de l’Économie et de la Finance")
-
 st.markdown("""
-
-Bienvenue sur cette plateforme interactive !  
-
+Bienvenue sur cette plateforme interactive !  
 Utilisez le menu latéral ou les onglets en haut pour naviguer entre les différentes sections :
-
 - **Dashboard** : Vue d'ensemble et indicateurs clés.
-
 - **Macro Indicateurs** : Analyse des indices macroéconomiques.
-
 - **Sector Indicateurs** : Visualisation des indicateurs par secteur.
-
 - **Finances Publiques** : Analyse des budgets et dépenses publiques.
-
 - **Simulation Avancée** : Scénarios économiques interactifs.
-
 - **Tutoriels** : Ressources pédagogiques et tutoriels.
-
 - **Analyse** : Outils d'analyse avancée (corrélation, régression).
-
 - **Quiz** : Testez vos connaissances grâce à un quiz interactif.
-
 """)
